719b50e18f5b7e2161a7a3c5c4800065181d90f6
Игорь Алиев <aliev.igor1@mail.ru>
2023-09-21 22:29:55 +0300

    Добавьте файлы проекта.

---
+++ kNN_Algorithm/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+class DataSet
+{
+    private string name;
+    private int size;
+
+    public DataSet()
+    {
+        this.name = "";
+        this.size = 0;
+    }
+
+    public DataSet (string name, int size)
+    {
+        this.name = name;
+        this.size = size;
+    }
+    
+}
+
+class Point_Class
+{
+
+}
+
+
+class Point
+{
+    private DataSet dataSet;
+    private int point_id;
+    private Point_Class p_class;
+
+    public Point() 
+    { 
+        dataSet = new DataSet();
+        point_id = 0;
+        p_class = new Point_Class();
+    }
+}
