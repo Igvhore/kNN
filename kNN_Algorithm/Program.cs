d7616c5bb35ac319619ba6b689727c1ad6329855
Игорь Алиев <aliev.igor1@mail.ru>
2023-09-22 14:40:14 +0300

    DataSet added

--- kNN_Algorithm/Program.cs
+++ kNN_Algorithm/Program.cs
@@ -1,41 +1,60 @@
-﻿
+﻿using CsvHelper;
+using System.Globalization;
 
+string dataSetPath = "C:\\Users\\aliev\\Downloads\\DataSet.csv";
+
+using (var reader = new StreamReader(dataSetPath))
+
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    var records = csv.GetRecords<Point>();
+
+    foreach (var record in records)
+        Console.WriteLine(record.Region);
+}
 
 class DataSet
 {
-    private string name;
-    private int size;
+    private string _name { get; set; }
+    private int _size { get; set; }
+    private Point[] _points { get; set; }
 
     public DataSet()
     {
-        this.name = "";
-        this.size = 0;
+        _name = "";
+        _size = 0;
+        _points = new Point[0];
     }
 
     public DataSet (string name, int size)
     {
-        this.name = name;
-        this.size = size;
+        _name = name;
+        _size = size;
+        _points = new Point[size];
     }
-    
+
 }
 
 class Point_Class
 {
 
+    public Point_Class()
+    {
+        
+    }
 }
 
 
 class Point
 {
-    private DataSet dataSet;
-    private int point_id;
-    private Point_Class p_class;
-
-    public Point() 
-    { 
-        dataSet = new DataSet();
-        point_id = 0;
-        p_class = new Point_Class();
-    }
+    public string Class { get; set; }
+    public string Sex { get; set; }
+    public string Sport { get; set; }
+    public string Job { get; set; }
+    public string HeartDisease { get; set; }
+    public string OwlOrLark { get; set; }
+    public string Milk { get; set; }
+    public string WakeUpTime { get; set; }
+    public string Region { get; set; }
+    public string SleepTime { get; set; }
 }
